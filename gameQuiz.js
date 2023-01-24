573a76cb48553680b6f6ce9958cd6c8ad3994dec
Pedro Henrique <pedrohgbarbosa@hotmail.com>
2023-01-24 11:08:50 -0300

    começou a rodar

--- gameQuiz.js
+++ gameQuiz.js
@@ -4,77 +4,92 @@ class Game {
       {
         Pergunta: "Qual a marca de automoveis mais segura do mundo ?",
         A: "Ford",
-        correct: false,
+
         B: "Peugeot",
-        correct: false,
+
         C: "Volvo",
-        correct: true,
+
         D: "VolksWagen",
-        correct: false,
+        correct: "Volvo",
       },
 
       {
         Pergunta:
           "Qual a marca de automoveis que criou o dispositivo de segurança (cinto de 3 pontos), e liberou a patente para as outras marcas ?",
         A: "GeneralMotors",
-        correct: false,
+
         B: "Citröen",
-        correct: false,
+
         C: "VolksWagen",
-        correct: false,
+
         D: "Volvo",
-        correct: true,
+        correct: "Volvo",
       },
 
       {
         Pergunta: "Quem desenvolveu o primeiro automóvel ?",
         A: "Pedro Henrique",
-        correct: false,
+
         B: "Enzo Ferrari",
-        correct: false,
+
         C: "Ferruccio Lamborghini",
-        correct: false,
+
         D: "Karl Benz",
-        correct: true,
+        correct: "Karl Benz",
       },
 
       {
         Pergunta: "Qual é a marca de carros que mais vende no mundo? ",
         A: "Toyota",
-        correct: true,
+
         B: "Honda",
-        correct: false,
+
         C: "VolksWagen",
-        correct: false,
+
         D: "Subaru",
-        correct: false,
+        correct: "Toyota",
       },
 
       {
         Pergunta: "Qual o carro mais caro já vendido no mundo (até 2022) ?",
         A: "Ferrari LaFerrari",
-        correct: false,
+
         B: "Bugatti La Voiture Noire",
-        correct: true,
+
         C: "Mercedes-AMG One",
-        correct: false,
+
         D: "Rolls-Royce Sweptail",
-        correct: false,
+
+        correct: "Bugatti La Voiture Noire",
       },
     ];
+    this.round = 0;
+    this.Pergunta = this.perguntas[this.round];
 
     //console.log(this.perguntas[i].Pergunta);
     //}
   }
   genBoard(board) {
-    for (let i = 0; i < this.perguntas.length; i++) {
-      const card = document.createElement("div");
-      card.classList.add("card");
-      board.appendChild(card);
-    }
-  }
+    const card = document.createElement("div");
+    card.classList.add("card");
+    let h1 = document.createElement("h1");
+    let btn1 = document.createElement("button");
+    let btn2 = document.createElement("button");
+    let btn3 = document.createElement("button");
+    let btn4 = document.createElement("button");
 
-  //questions() {
-  // for (let i = 0; i < this.perguntas.length; i++) {
-  //}
+    h1.innerText = this.perguntas.Pergunta;
+    btn1.innerText = this.Pergunta.A;
+    btn2.innerText = this.Pergunta.B;
+    btn3.innertext = this.Pergunta.C;
+    btn4.innerText = this.Pergunta.D;
+
+    card.appendChild(h1);
+    card.appendChild(btn1);
+    card.appendChild(btn2);
+    card.appendChild(btn3);
+    card.appendChild(btn4);
+
+    board.appendChild(card);
+  }
 }
