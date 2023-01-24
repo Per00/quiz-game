c4b2be3a11f7c9efcee4f03ea3cc061f2b2f7ebe
Pedro Henrique <pedrohgbarbosa@hotmail.com>
2023-01-24 08:49:38 -0300

    true or false

--- gameQuiz.js
+++ gameQuiz.js
@@ -4,42 +4,62 @@ class Game {
       {
         Pergunta: "Qual a marca de automoveis mais segura do mundo ?",
         A: "Ford",
+        correct: false,
         B: "Peugeot",
-        C: "Volvo", // true
+        correct: false,
+        C: "Volvo",
+        correct: true,
         D: "VolksWagen",
+        correct: false,
       },
 
       {
         Pergunta:
           "Qual a marca de automoveis que criou o dispositivo de segurança (cinto de 3 pontos), e liberou a patente para as outras marcas ?",
         A: "GeneralMotors",
+        correct: false,
         B: "Citröen",
+        correct: false,
         C: "VolksWagen",
-        D: "Volvo", // true
+        correct: false,
+        D: "Volvo",
+        correct: true,
       },
 
       {
         Pergunta: "Quem desenvolveu o primeiro automóvel ?",
         A: "Pedro Henrique",
+        correct: false,
         B: "Enzo Ferrari",
+        correct: false,
         C: "Ferruccio Lamborghini",
-        D: "Karl Benz", // true
+        correct: false,
+        D: "Karl Benz",
+        correct: true,
       },
 
       {
         Pergunta: "Qual é a marca de carros que mais vende no mundo? ",
-        A: "Toyota", // true
+        A: "Toyota",
+        correct: true,
         B: "Honda",
+        correct: false,
         C: "VolksWagen",
+        correct: false,
         D: "Subaru",
+        correct: false,
       },
 
       {
         Pergunta: "Qual o carro mais caro já vendido no mundo (até 2022) ?",
         A: "Ferrari LaFerrari",
-        B: "Bugatti La Voiture Noire", // true
+        correct: false,
+        B: "Bugatti La Voiture Noire",
+        correct: true,
         C: "Mercedes-AMG One",
+        correct: false,
         D: "Rolls-Royce Sweptail",
+        correct: false,
       },
     ];
 
