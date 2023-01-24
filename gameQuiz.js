cad8364fedabeafead4bf53d89e46eb432a573bc
Pedro Henrique <pedrohgbarbosa@hotmail.com>
2023-01-23 22:43:52 -0300

    m2

--- gameQuiz.js
+++ gameQuiz.js
@@ -5,37 +5,56 @@ class Game {
         Pergunta: "Qual a marca de automoveis mais segura do mundo ?",
         A: "Ford",
         B: "Peugeot",
-        C: "Volvo",
+        C: "Volvo", // true
         D: "VolksWagen",
       },
 
       {
-        Pergunta: "Qual a marca de automoveis mais segura do mundo ?",
-        A: "Ford",
-        B: "Peugeot",
-        C: "Volvo",
-        D: "VolksWagen",
+        Pergunta:
+          "Qual a marca de automoveis que criou o dispositivo de segurança (cinto de 3 pontos), e liberou a patente para as outras marcas ?",
+        A: "GeneralMotors",
+        B: "Citröen",
+        C: "VolksWagen",
+        D: "Volvo", // true
       },
 
       {
-        Pergunta: "Qual a marca de automoveis mais segura do mundo ?",
-        A: "Ford",
-        B: "Peugeot",
-        C: "Volvo",
-        D: "VolksWagen",
+        Pergunta: "Quem desenvolveu o primeiro automóvel ?",
+        A: "Pedro Henrique",
+        B: "Enzo Ferrari",
+        C: "Ferruccio Lamborghini",
+        D: "Karl Benz", // true
+      },
+
+      {
+        Pergunta: "Qual é a marca de carros que mais vende no mundo? ",
+        A: "Toyota", // true
+        B: "Honda",
+        C: "VolksWagen",
+        D: "Subaru",
+      },
+
+      {
+        Pergunta: "Qual o carro mais caro já vendido no mundo (até 2022) ?",
+        A: "Ferrari LaFerrari",
+        B: "Bugatti La Voiture Noire", // true
+        C: "Mercedes-AMG One",
+        D: "Rolls-Royce Sweptail",
       },
     ];
-    //for (let i = 0; i < this.perguntas.length; i++) {
+
     //console.log(this.perguntas[i].Pergunta);
     //}
   }
-  genBoard(boardQuiz) {
-    for (let i = 0; i < this.perguntas; i++) {
-      const notes = document.createElement("div");
-      notes.classList.add("notes");
-
-      boardQuiz.appendChild(notes);
-      // criando 5 divs, as perguntas
+  genBoard(board) {
+    for (let i = 0; i < this.perguntas.length; i++) {
+      const card = document.createElement("div");
+      card.classList.add("card");
+      board.appendChild(card);
     }
   }
+
+  //questions() {
+  // for (let i = 0; i < this.perguntas.length; i++) {
+  //}
 }
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const btnStart = document.getElementById("btn-start");
 const header = document.getElementById("header");
-const boardQuiz = document.getElementById("Board-quiz");
+const board = document.getElementById("board-Quiz");
 
 btnStart.addEventListener("click", () => {
   header.classList.add("hidden");
 
   const game = new Game();
 
-  game.genBoard(boardQuiz);
+  game.genBoard(board);
 });
